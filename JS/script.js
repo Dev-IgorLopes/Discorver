750922f51b010643c7ae7fc854a4cf7c6718959f
Dev-IgorLopes <igorlopes.adm1@gmail.com>
2023-06-26 23:18:06 -0300

    remove

--- JS/script.js
+++ JS/script.js
@@ -5,11 +5,10 @@ function toggleMode(){
   const img = document.querySelector("#profile img")
 
   if(html.classList.contains('light')){
-    img.setAttribute('src', './assets/avatar-light.png')
-    img.setAttribute('alt', 'Foto light')
+    img.setAttribute('src', './assets/avatar-light.png');
+   
   }else{
-    img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute("alt", "Foto Dark")
+    img.setAttribute("src", "./assets/avatar.png");
   }
 
 }
